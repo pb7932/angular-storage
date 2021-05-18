e48e20e626fd50169c63f5a9db76b5fd5193efd4
pb7932 <pebe1928@gmail.com>
2021-05-18 16:45:45 +0200

    Adding basic routes, global styles, 404 component, dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFound404Component } from './components/dashboard/page-not-found404/page-not-found404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: '**', component: PageNotFound404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFound404Component } from './components/dashboard/page-not-found404/page-not-found404.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFound404Component,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
