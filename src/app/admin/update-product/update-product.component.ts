ccf26509836992f94250297cb4ed5c540d61c880
pb7932 <pebe1928@gmail.com>
2021-06-03 10:14:53 +0200

    Building a form and implementing update logic

--- src/app/admin/update-product/update-product.component.ts
+++ src/app/admin/update-product/update-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Product } from 'src/app/services/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-update-product',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-product.component.css']
 })
 export class UpdateProductComponent implements OnInit {
-
-  constructor() { }
+  product?: Product;
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getProduct();
   }
 
+  getProduct(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id).subscribe(p => this.product = p);
+  }
+
+  onSubmit(): void {
+    console.log(this.product);
+    this.productService.updateProduct(this.product!).subscribe();
+  }
 }
