351192fea232e3bd06ea4b8e1450b39c761b0311
pb7932 <pebe1928@gmail.com>
2021-06-08 18:27:19 +0200

    Displaying a table of products in manage-products component

--- src/app/admin/manage-storage/manage-storage.component.ts
+++ src/app/admin/manage-storage/manage-storage.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from 'src/app/services/product';
+import { ProductService } from 'src/app/services/product.service';
+
 @Component({
   selector: 'app-manage-storage',
   templateUrl: './manage-storage.component.html',
   styleUrls: ['./manage-storage.component.css']
 })
 export class ManageStorageComponent implements OnInit {
-
-  constructor() { }
+  products!: Product[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(products => {this.products = products});
   }
 
 }
